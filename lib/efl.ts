81771d7aeae76ab96b5815c8ded811edfe2774f1
bllfield <bllfield32@gmail.com>
2025-10-23 16:15:57 -0500

    Fix html-to-text import: use intermediate variable to bypass TypeScript analysis

--- lib/efl.ts
+++ lib/efl.ts
@@ -66,7 +66,8 @@ async function extractHtmlText(htmlBuf: Buffer): Promise<{ text: string; $: Chee
   const html = htmlBuf.toString('utf8');
   const cheerio = (await import('cheerio')).load;
   const $ = cheerio(html);
-  const { htmlToText } = await import('html-to-text') as any;
+  const htmlToTextModule = await import('html-to-text') as any;
+  const { htmlToText } = htmlToTextModule;
   const text = htmlToText(html, {
     wordwrap: false,
     selectors: [
