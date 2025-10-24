140d06f7007f36f9956378229a2b92669ca39d53
bllfield <bllfield32@gmail.com>
2025-10-23 22:02:53 -0500

    Fix admin login logo size to 64x64 (2x header/footer size)

--- app/admin-login/page.tsx
+++ app/admin-login/page.tsx
@@ -73,7 +73,7 @@ function AdminLoginContent() {
 
       <div className="max-w-md w-full space-y-8">
         <div>
-          <div className="mx-auto h-32 w-32 flex items-center justify-center">
+          <div className="mx-auto h-16 w-16 flex items-center justify-center">
             <Image
               src="/IntelliWatt Logo.png"
               alt="IntelliWatt Logo"
