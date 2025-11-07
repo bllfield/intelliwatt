6343506618fa53cd90c7aade0630d8f54393fcbd
Brian Littlefield <brian.littlefield@intellipath-solutions.com>
2025-11-07 15:08:21 -0600

    Remove ping endpoints

--- app/api/ping.txt/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-  return new NextResponse('OK', {
-    status: 200,
-    headers: { 'content-type': 'text/plain; charset=utf-8' },
-  });
-}
