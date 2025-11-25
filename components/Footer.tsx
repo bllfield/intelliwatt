51b2b8d9f7f1850b0105a2bcd7113f7ffbe8e4e4
Brian Littlefield <brian.littlefield@intellipath-solutions.com>
2025-11-25 04:22:40 -0600

    Update footer social icons to Facebook, X, Instagram, YouTube, TikTok

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -20,19 +20,59 @@ export default function Footer() {
               Smart algorithms find the perfect plan for your unique usage patterns.
             </p>
             <div className="flex space-x-4">
-              <a href="#" className="text-brand-white transition-colors hover:text-brand-blue">
+              <a
+                href="https://www.facebook.com/IntelliWatt"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="IntelliWatt on Facebook"
+                className="text-brand-white transition-colors hover:text-brand-blue"
+              >
                 <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
-                  <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
+                  <path d="M22.675 0H1.325C.594 0 0 .592 0 1.325v21.35C0 23.406.594 24 1.325 24H12.82v-9.294H9.692v-3.622h3.128V8.414c0-3.1 1.894-4.793 4.66-4.793 1.325 0 2.463.099 2.795.143v3.24h-1.917c-1.504 0-1.796.715-1.796 1.765v2.31h3.587l-.467 3.622h-3.12V24h6.116C23.406 24 24 23.406 24 22.675V1.325C24 .592 23.406 0 22.675 0z" />
                 </svg>
               </a>
-              <a href="#" className="text-brand-white transition-colors hover:text-brand-blue">
+              <a
+                href="https://twitter.com/IntelliWatt"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="IntelliWatt on X"
+                className="text-brand-white transition-colors hover:text-brand-blue"
+              >
                 <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
-                  <path d="M22.46 6c-.77.35-1.6.58-2.46.69.88-.53 1.56-1.37 1.88-2.38-.83.5-1.75.85-2.72 1.05C18.37 4.5 17.26 4 16 4c-2.35 0-4.27 1.92-4.27 4.29 0 .34.04.67.11.98C8.28 9.09 5.11 7.38 3 4.79c-.37.63-.58 1.37-.58 2.15 0 1.49.75 2.81 1.91 3.56-.71 0-1.37-.2-1.95-.5v.03c0 2.08 1.48 3.82 3.44 4.21a4.22 4.22 0 0 1-1.93.07 4.28 4.28 0 0 0 4 2.98 8.521 8.521 0 0 1-5.33 1.84c-.34 0-.68-.02-1.02-.06C3.44 20.29 5.7 21 8.12 21 16 21 20.33 14.46 20.33 8.79c0-.19 0-.37-.01-.56.84-.6 1.56-1.36 2.14-2.23z" />
+                  <path d="M17.5 3H21L14.2 10.6 22 21h-5.8l-4.6-6.7L6.2 21H0l8.1-9.84L1.1 3h5.95l4.22 6.11L17.5 3z" />
                 </svg>
               </a>
-              <a href="#" className="text-brand-white transition-colors hover:text-brand-blue">
+              <a
+                href="https://www.instagram.com/IntelliWatt"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="IntelliWatt on Instagram"
+                className="text-brand-white transition-colors hover:text-brand-blue"
+              >
                 <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
-                  <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
+                  <path d="M7 2h10a5 5 0 0 1 5 5v10a5 5 0 0 1-5 5H7a5 5 0 0 1-5-5V7a5 5 0 0 1 5-5zm0 2a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V7a3 3 0 0 0-3-3H7zm5 2.5a4.5 4.5 0 1 1 0 9 4.5 4.5 0 0 1 0-9zm0 2a2.5 2.5 0 1 0 .001 5.001 2.5 2.5 0 0 0-.001-5.001zm6.75-.9a1.05 1.05 0 1 1-2.1 0 1.05 1.05 0 0 1 2.1 0z" />
+                </svg>
+              </a>
+              <a
+                href="https://www.youtube.com/@IntelliWatt"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="IntelliWatt on YouTube"
+                className="text-brand-white transition-colors hover:text-brand-blue"
+              >
+                <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+                  <path d="M23.498 6.186a3.003 3.003 0 0 0-2.11-2.12C19.691 3.5 12 3.5 12 3.5s-7.691 0-9.388.566a3.003 3.003 0 0 0-2.11 2.12A31.118 31.118 0 0 0 .5 12a31.118 31.118 0 0 0 .002 5.814 3.003 3.003 0 0 0 2.11 2.12C4.309 20.5 12 20.5 12 20.5s7.691 0 9.388-.566a3.003 3.003 0 0 0 2.11-2.12A31.118 31.118 0 0 0 23.5 12a31.118 31.118 0 0 0-.002-5.814zM9.75 15.5v-7l6 3.5-6 3.5z" />
+                </svg>
+              </a>
+              <a
+                href="https://www.tiktok.com/@IntelliWatt"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="IntelliWatt on TikTok"
+                className="text-brand-white transition-colors hover:text-brand-blue"
+              >
+                <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+                  <path d="M21.54 7.2a4.76 4.76 0 0 1-2.91-.99V15a6.03 6.03 0 1 1-6.91-5.91v3.11a2.49 2.49 0 1 0 1.79 2.39V2h3.12a4.74 4.74 0 0 0 4.91 4.2z" />
                 </svg>
               </a>
             </div>
