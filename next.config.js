85d0fcdc9c0c41a3fd842019cf5a563bd11fe0ff
Brian Littlefield <brian.littlefield@intellipath-solutions.com>
2025-11-30 19:17:41 -0600

    Bundle custom Prisma clients in Next.js build

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
+  experimental: {
+    serverComponentsExternalPackages: [
+      '@prisma/client',
+      '@prisma/current-plan-client',
+      '@prisma/usage-client',
+    ],
+  },
 };
 
 module.exports = nextConfig; 
